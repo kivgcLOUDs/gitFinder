903c214168a4527c1170e632ace02bee546c9432
his-imag8 <ekanemdaniel60@gmail.com>
2026-06-05 22:54:34 +0100

    First ever commit setting up layout

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+const repo = document.querySelector(".public-repo");
+const img = document.querySelector(".profile-photo");
+const followers = document.querySelector(".followers");
+const bio = document.querySelector(".bio");
+
+async function gitHubProfile(username) {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+
+    if (!res.ok) throw new Error();
+
+    const data = await res.json();
+
+    ////////////////////////
+    /// HTML CHANGES
+    img.src = data.avatar_url;
+    repo.textContent = data.public_repos;
+    followers.textContent = data.followers;
+    bio.textContent = data.bio;
+
+    //////////////////////////
+    //
+    console.log(data);
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+gitHubProfile("Deviant08");
